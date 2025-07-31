f652108cfc6748e49296d47953f8e631a02ed312
AyadAllawi <AyadAllawi315@gmail.com>
2025-07-31 11:48:23 +0700

    tugas_1_flutter

---
+++ android/app/src/main/kotlin/com/example/ppkd_batch_3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ppkd_batch_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/start.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class StartWidget extends StatelessWidget {
+  const StartWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "MY PROFILE",
+          style: TextStyle(
+            fontFamily: "Rubik",
+            color: Color.fromARGB(255, 255, 255, 254),
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 5, 73, 151),
+        actions: const [
+          Icon(Icons.favorite, color: Color.fromARGB(255, 255, 3, 3)),
+          Icon(Icons.message, color: Color.fromARGB(255, 255, 255, 255)),
+        ],
+        centerTitle: true,
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+
+      // backroundColor: Colors.black
+      body: Column(
+        // MainAxis center untuk posisi ketengah
+        // mainAxisAlignment: MainAxisAlignment.center,
+        // mainAxisAlignment: MainAxisAlignment.end,
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "AYAD ALLAWI",
+            style: TextStyle(
+              fontSize: 25,
+              color: const Color.fromARGB(255, 0, 0, 0),
+              fontFamily: "Anton",
+            ),
+          ),
+
+          SizedBox(height: 10),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.location_on,
+                color: const Color.fromARGB(255, 4, 67, 240),
+              ),
+              Text("DKI JAKARTA", style: TextStyle(fontFamily: "Monoton")),
+            ],
+          ),
+
+          SizedBox(height: 10),
+
+          Text(
+            "Nama Saya adalah Ayad Allawi, saya sekarang sedang belajar UI Flutter, ya sedikit bingung, tetapi tidak mematahkan semangat saya ",
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
