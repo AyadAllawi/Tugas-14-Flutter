3ad8476c42795114936b9b38a81ac5bc29db71ee
AyadAllawi <AyadAllawi315@gmail.com>
2025-08-07 14:39:27 +0700

    tugas_flutter_6

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ppkd_batch_3/Day_8/Interaction.dart';
+import 'package:ppkd_batch_3/Day_9/login.dart';
 // import 'package:ppkd_batch_3/start.dart';
 
 void main() {
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Interaction(),
+      home: login(),
     );
   }
 }
